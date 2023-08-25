28e3f51b3c5913da811d28cfe3fb6af52862a9a0
AhmedHdeawy <ahmedhdeawy@gmail.com>
2023-08-25 06:48:34 +0300

    create order items table

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderItem extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $fillable = ['price', 'quantity', 'order_id', 'product_id'];
+}
